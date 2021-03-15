7fb0808a2f5099f8f1afb297a942379a7e461916
faraz <faraz.kazi22@gmail.com>
2021-03-15 14:28:38 -0700

    Built frame and created buttons

--- src/main/ui/GraphicalWorkoutLogApp.java
+++ src/main/ui/GraphicalWorkoutLogApp.java
@@ -5,26 +5,56 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.FileNotFoundException;
 
 public class GraphicalWorkoutLogApp extends JFrame {
     private static final String JSON_STORE = "./data/workouts.json";
-    private WorkoutLog log;
-
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
+
+    private WorkoutLog log;
+
+
+    private JFrame homeFrame = new JFrame("Workout Log");
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;
+
 
     public GraphicalWorkoutLogApp() throws FileNotFoundException {
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
         initializeFields();
-        initializeGraphics();
+        initializeFrame();
     }
 
     private void initializeFields() {
+        log = new WorkoutLog();
     }
 
-    private void initializeGraphics() {
+    private void initializeFrame() {
+        homeFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addButtons();
+        homeFrame.setVisible(true);
+        
+    }
 
+    private void addButtons() {
+        JPanel buttonPanel = new JPanel();
+
+        JButton newButton = new JButton("Start workout");
+        JButton viewButton = new JButton("View your log");
+        JButton loadButton = new JButton("Load workouts");
+        JButton saveButton = new JButton("Save workouts");
+        JButton quitButton = new JButton("Quit");
+
+        buttonPanel.add(newButton);
+        buttonPanel.add(viewButton);
+        buttonPanel.add(loadButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(quitButton);
+
+        homeFrame.add(buttonPanel);
     }
 }
